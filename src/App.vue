2b415b6089f11f1176e83744d3fb46176df0866c
salvomel <salv.mel@gmail.com>
2021-12-17 15:34:43 +0100

    Title + Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Title />
+    <Hero />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Title from "./components/Title.vue";
+import Hero from "./components/Hero.vue";
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Title,
+    Hero
   },
 };
 </script>
@@ -19,10 +23,12 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="../assets/img/nebulosa.jpg" alt="Nebulosa">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Hero",
+    };
+</script>
+
+<style scoped>
+    img {
+        width: 100%;
+        margin-top: 20px;
+    }
+</style>
